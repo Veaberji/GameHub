3a947ad75ac1540fe1d346c9074514ca366d64a7
Pavel Morozov <veaberji@gmail.com>
2023-05-26 15:12:18 +0400

    add search games

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
 import ColorModeSwitch from './ColorModeSwitch';
+import SearchInput from './SearchInput';
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="0.6rem">
+    <HStack padding="0.6rem">
       <Image src={logo} boxSize="3.75rem" alt="Logo" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
